c53120855e15e9ecf65bd102bb867dcafd6085f2
KimChol <kimchol2ece@outlook.com>
2020-11-26 17:03:54 +0800

    电影页面

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 
@@ -7,6 +7,36 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route('/index')
+def home():
+    return index()
+
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    cur.close()
+    con.close()
+    return render_template("movie.html",movies = datalist)
+
+@app.route('/score')
+def score():
+    return render_template("score.html")
+
+@app.route('/word')
+def word():
+    return render_template("word.html")
+
+@app.route('/team')
+def team():
+    return render_template("team.html")
 
 if __name__ == '__main__':
     app.run()
